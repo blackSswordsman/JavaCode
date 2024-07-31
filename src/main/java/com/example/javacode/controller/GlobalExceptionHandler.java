5316641fb428b4a1535a3e233de19855b2622fc0
Daniia <edikhanova.daniya@gmail.com>
2024-07-31 13:40:41 +0300

    Javadoc

--- src/main/java/com/example/javacode/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/javacode/controller/GlobalExceptionHandler.java
@@ -16,13 +16,24 @@ import org.springframework.web.context.request.WebRequest;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Глобальный обработчик exceptions приложения.
+ * Если возникают иключения типа MethodArgumentNotValidException,ApplicationException,
+ * он попытается вернуть форматированный вывод в response
+ */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, MethodArgumentNotValidException.class})
+    /**
+     *
+     * Если в процессе валидации запроса пользователя или в процессе валидации аргументов какого-либо метода,
+     * произойдет ошибка, которая выбрасывает MethodArgumentNotValidException,
+     * вернет в респонсе форматированный вывод об этой ошибке.
+     * @return список филдов, которые не прошли валидацию, и ошибки валидации
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
@@ -31,8 +42,15 @@ public class GlobalExceptionHandler {
         });
         return errors;
     }
+
+    /**
+     *
+     * Если в процессе работы программы произойдет исключение приложения, которое мы специально выбрасываем
+     * в некоторых случаях, вернется форматированный вывод (иначе код ошибки без данного метода) в респонсе, и
+     * установит у респонса статус ответа из аннотации @ResponseStatus.
+     */
     @ExceptionHandler(ApplicationException.class)
-    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+    protected ResponseEntity<Object> handleConflict(ApplicationException ex, WebRequest request) {
         ResponseStatus responseStatus = AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class);
         if (responseStatus != null) {
             HttpStatus status = responseStatus.value();
